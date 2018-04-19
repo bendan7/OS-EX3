5c72a94df9874c5db0812a865d63a5e2f2b505bf
ben <bendan7@gmail.com>
2018-04-19 19:53:49 +0300

    all work, and there is an exemple of shared mem read and write

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unistd.h>
 #include "Menu.h"
 #include "Menu.cpp"
 #include "Dish.h"
@@ -23,40 +24,60 @@ int main(void) {
     char  *segmem1ptr;
 
 
-//    clock_t start = clock();
-//    clock_t now ;
 
 
-
-    cout << "====Simulation arguments====\n";
-    cout<< "simulation time: "<< simuTime<<"\n";
-    cout<< "Menu items count: "<< numOfDish << "\n";
-    cout << "Customers count: "<< coustNum<<"\n";
-    cout<< "Waiter count: 1"<< waiterNum<<"\n";
+//    cout << "====Simulation arguments====\n";
+//    cout<< "simulation time: "<< simuTime<<"\n";
+//    cout<< "Menu items count: "<< numOfDish << "\n";
+//    cout << "Customers count: "<< coustNum<<"\n";
+//    cout<< "Waiter count: 1"<< waiterNum<<"\n";
 
     //creation of shared memory
     sharedMemID = shmget(sharedMemKEY, 128 ,IPC_CREAT|IPC_EXCL|0666);
 
     //if shared memory allocated is faild
-    if(sharedMemID<0){
+    if(sharedMemID==-1){
         cout<< "\nshared memory allocated faild!";
         return 1;
     }
 
+
     segmem1ptr = static_cast<char *>(shmat(sharedMemID, 0, 0));
     segmem1ptr[0]= '!';
 
+    int pid =fork();
+    if(pid<0){
+        cout <<"\n fork is faild!";
+        return 1;
+    }
 
 
-    cout << sharedMemID;
+    if (pid==0){
+        //son section
+        segmem1ptr[0]='a';
+        exit(0);
+
+    } else{
+        //father section
+
+        sleep(2);
+        cout << segmem1ptr[0];
+
+
+    }
+
+
+
 
 
     //Menu h(3);
-
    // h.print();
 
 
-
+    // close the shared memory
+    if(shmctl (sharedMemID, IPC_RMID, 0)==-1){
+        cout<<"\n Shaerd mem didnt delete!!";
+    }
 
     return 0;
 }
