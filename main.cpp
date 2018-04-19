07f63faafc991c71b0b2f14039d8e65245b49871
ben <bendan7@gmail.com>
2018-04-19 19:14:17 +0300

    all work, just play with ipc

--- main.cpp
+++ main.cpp
@@ -3,24 +3,59 @@
 #include "Menu.cpp"
 #include "Dish.h"
 #include "Dish.cpp"
+#include "sys/types.h"
+#include "sys/ipc.h"
+#include "sys/shm.h"
 
 using namespace std;
 
 int main(void) {
 
+    //init values
+    int simuTime= 15;
+    int numOfDish =5;
+    int coustNum= 2;
+    int waiterNum = 1;
 
-//    if(argc!=5){
-//        cout<<"Input arguments are not valid!\n";
-//        return 1;
-//    }
-//    else{
-//
-//    }
+    //shared memory
+    key_t sharedMemKEY = ftok(".",'a');
+    int sharedMemID;
+    char  *segmem1ptr;
 
 
-    Menu h(5);
+//    clock_t start = clock();
+//    clock_t now ;
 
-    h.print();
+
+
+    cout << "====Simulation arguments====\n";
+    cout<< "simulation time: "<< simuTime<<"\n";
+    cout<< "Menu items count: "<< numOfDish << "\n";
+    cout << "Customers count: "<< coustNum<<"\n";
+    cout<< "Waiter count: 1"<< waiterNum<<"\n";
+
+    //creation of shared memory
+    sharedMemID = shmget(sharedMemKEY, 128 ,IPC_CREAT|IPC_EXCL|0666);
+
+    //if shared memory allocated is faild
+    if(sharedMemID<0){
+        cout<< "\nshared memory allocated faild!";
+        return 1;
+    }
+
+    segmem1ptr = static_cast<char *>(shmat(sharedMemID, 0, 0));
+    segmem1ptr[0]= '!';
+
+
+
+    cout << sharedMemID;
+
+
+    //Menu h(3);
+
+   // h.print();
+
+
 
 
     return 0;
